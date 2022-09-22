daefb23b3818dc36ed6c0c62a062ec9e0bf1c700
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2022-09-22 06:36:50 -0400

    UI test finished

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+const APP_CONTAINTER = document.getElementById("#app");
+const MAX_SECONDS = 59;
+const MAX_MINUTES = 59;
+const MAX_HOURS = 12;
+const MIN_TIME = 0;
+
+const TIMER_INIT = (hours, minutes, seconds) =>{
+
+}

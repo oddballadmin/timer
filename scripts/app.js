663ca5f7ee30554b12b540c5aeb3b1e31caa98b7
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2022-09-22 07:05:46 -0400

    timer setup functionality added

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,14 @@ const MAX_SECONDS = 59;
 const MAX_MINUTES = 59;
 const MAX_HOURS = 12;
 const MIN_TIME = 0;
+const SUBMIT_BTN = document.getElementById("timer-submit");
+let secondsSet, minutesSet, hoursSet;
+SUBMIT_BTN.addEventListener('click', () => {
+    let timerSetLbl = document.getElementById("timer-amount");
+    secondsSet = document.getElementById("Iseconds").value;
+    minutesSet = document.getElementById("Iminutes").value;
+    hoursSet = document.getElementById("Ihours").value;
+    timerSetLbl.textContent = (`${hoursSet}:${minutesSet}:${secondsSet}`);
+});
 
-const TIMER_INIT = (hours, minutes, seconds) =>{
 
-}

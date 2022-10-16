72711313b186809973399ec19677fdbbb9c03fe3
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2022-10-16 00:16:25 -0400

    refrasctored app.js

--- scripts/app.js
+++ scripts/app.js
@@ -2,34 +2,33 @@ const APP_CONTAINTER = document.getElementById("#app");
 const MAX_SECONDS = 59;
 const MAX_MINUTES = 59;
 const MAX_HOURS = 12;
+const TIME_INCREMENT = 10;
 const MIN_TIME = 0;
 const SUBMIT_BTN = document.getElementById("timer-submit");
 const RESET_BTN = document.getElementById("timer-reset");
-let secondsSet, minutesSet, hoursSet;
+
+let secondLabelDom = document.getElementById("seconds");
+let minuteLabelDom = document.getElementById("minutes");
+let hourLabelDom = document.getElementById("hours");
+
+let secondInputDom = document.getElementById("Iseconds");
+let minuteInputDom = document.getElementById("Iminutes");
+let hourInputDom = document.getElementById("Ihours");
+
+let timerSetLbl = document.getElementById("timer-amount");
+
+
 RESET_BTN.addEventListener('click', () => {
-    document.getElementById("Iseconds").value = 0;
-    document.getElementById("Iminutes").value = 0;
-    document.getElementById("Ihours").value = 0;
+    secondInputDom.value = 0;
+    minuteInputDom.value = 0;
+    hourInputDom.value = 0;
 });
 
 SUBMIT_BTN.addEventListener('click', () => {
-    let timerSetLbl = document.getElementById("timer-amount");
-    secondsSet = document.getElementById("Iseconds").value;
-    minutesSet = document.getElementById("Iminutes").value;
-    hoursSet = document.getElementById("Ihours").value;
+    let secondsSet = secondInputDom.value;
+    let minutesSet = minuteInputDom.value;
+    let hoursSet = hourInputDom.value;
     timerSetLbl.textContent = (`${hoursSet}:${minutesSet}:${secondsSet}`);
     startCountDown(hoursSet,minutesSet,secondsSet);
 });
-const updateTime = (h,m,s)=> {
-    document.getElementById("Iseconds").value = s;
-    document.getElementById("Iminutes").value = m;
-    document.getElementById("Ihours").value = h;
-};
-
-const startCountDown = (h, m, s) => {
-    let isTimeUp = false;
-    while(!isTimeUp){
-
-    }
-};
 
